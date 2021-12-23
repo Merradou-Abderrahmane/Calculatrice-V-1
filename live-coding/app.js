69343aa4eb6c8b5e40a1d1338945a5acc7c120fb
Merradou-Abderrahmane <abderahmane.merradou@gmail.com>
2021-12-23 09:50:14 +0100

    save display  of first number

---
+++ live-coding/app.js
@@ -0,0 +1,44 @@
+// declaring variabels
+var a;
+var aString;
+var b;
+var bString;
+var operation;
+var total;
+
+function onClickNumber (number){
+    if ( a == undefined) {
+        if (aString == undefined) {
+            aString = "";
+        }
+       
+        aString += number 
+    }
+    
+    if ( b == undefined) {
+        if(bString == undefined){
+            bString = "";
+        }
+        bString += number;
+    }
+    display();
+
+}
+
+function display(number){
+    var display = document.getElementById("display");
+    display.value = "" ;
+  if( a != undefined && b != undefined && operation != undefined){
+      display.value = number;
+  }
+
+  else {
+    if ( aString != undefined){
+     display.value = aString;
+    }
+  }
+  
+
+}
+
+function onClickOperation
