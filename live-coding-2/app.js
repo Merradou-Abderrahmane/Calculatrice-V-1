e32071e45b875c76ebd1fb60a8f54b3e14bbe2cb
Merradou-Abderrahmane <abderahmane.merradou@gmail.com>
2021-12-23 11:23:02 +0100

    add aString

--- live-coding-2/app.js
+++ live-coding-2/app.js
@@ -6,4 +6,22 @@ var bString;
 var operation;
 var total;
 
+function onClickNumber(number) {
+    if (a == undefined){
+        if (aString == undefined) {
+          aString = '';
+        }
+     aString += number
+    }
 
+    dispaly();
+}
+
+function dispaly(number){
+    var dispaly = document.getElementById("display");
+    dispaly.value = '';
+    
+    if (aString != undefined) {
+      dispaly.value += aString
+    }
+}
